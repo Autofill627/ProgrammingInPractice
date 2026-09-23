2f9b0f8abd42dc603168ff15882209c4d80c00c4
Tavey Ngenokesho <226008053ngenokesho@gmail.co>
2026-09-23 18:29:18 +0200

    Week 2 - Municipal Budget Calculator

---
+++ Week2/budget.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main()
+{
+    double revenue;
+    double expenses;
+    double balance;
+
+    printf("MUNICIPAL BUDGET CALCULATOR\n");
+    printf("---------------------------\n");
+
+    printf("Enter total revenue: ");
+    scanf("%lf", &revenue);
+
+    printf("Enter total expenses: ");
+    scanf("%lf", &expenses);
+
+    balance = revenue - expenses;
+
+    printf("\nRevenue: %.2f\n", revenue);
+    printf("Expenses: %.2f\n", expenses);
+    printf("Balance: %.2f\n", balance);
+
+    return 0;
+}
